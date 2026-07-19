8c7270f27bc10452397e530bed42819f422fb69a
Toby <therealtobes28@gmail.com>
2026-07-20 00:32:19 +0100

    feat: add main entry-point script with cog loading

---
+++ main.py
@@ -0,0 +1,39 @@
+"""Main entry point for the bot."""
+import os
+import logging
+
+from discord import Bot, InteractionContextType, ExtensionFailed
+
+import dotenv
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(stream_handler)
+
+dotenv.load_dotenv()
+
+bot = Bot(
+    auto_sync_commands=True,
+    default_command_contexts={
+        InteractionContextType.guild,
+        InteractionContextType.bot_dm,
+        InteractionContextType.private_channel
+    }
+)
+
+@bot.event
+async def on_ready():
+    """Event handler for when the bot is ready."""
+    print(f"Ready as {bot.user}")
+
+for filename in os.listdir("cogs"):
+    if filename.endswith(".py"):
+        try:
+            bot.load_extension(f"cogs.{filename[:-3]}")
+        except ExtensionFailed as e:
+            print(f"Failed to load extension {filename}: {e}")
+
+if __name__ == "__main__":
+    bot.run(os.getenv("TOKEN"))
