ac78971861fe988773c77a66cd0e160ab47940f1
Toby <therealtobes28@gmail.com>
2026-07-23 14:49:41 +0100

    feat(debug): add debug argparsing

--- src/main.py
+++ src/main.py
@@ -1,13 +1,18 @@
 """Main entry point for the bot."""
 import os
 import logging
+import argparse
 
 from discord import Bot, InteractionContextType, IntegrationType, ExtensionFailed
 
 import dotenv
 
+parser = argparse.ArgumentParser(description="Run the Discord bot.")
+parser.add_argument("--debug", "-d", action="store_true", help="Enable debug mode.")
+args = parser.parse_args()
+
 logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.DEBUG if args.debug else logging.INFO)
 stream_handler = logging.StreamHandler()
 stream_handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(stream_handler)
@@ -31,7 +36,7 @@ async def on_ready():
     """Event handler for when the bot is ready."""
     print(f"Ready as {bot.user}")
 
-for filename in os.listdir("cogs"):
+for filename in os.listdir(os.path.join(os.path.dirname(__file__), "cogs")):
     if filename.endswith(".py"):
         try:
             bot.load_extension(f"cogs.{filename[:-3]}")
